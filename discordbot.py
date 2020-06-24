09e069b2ea1e8c801617297a63162598f5a53023
amano2 <66359410+amano2@users.noreply.github.com>
2020-06-25 08:11:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,9 @@ TOKEN = os.environ['TOKEN']
 client = discord.Client()
 
 
+ID_CHANNEL_README = 725486353151819899 # 該当のチャンネルのID
+ID_ROLE_WELCOME = 719176372773453835 # 付けたい役職のID
+
 
 @client.event
 async def on_message(message):
@@ -26,6 +29,29 @@ async def on_message(message):
             description=f"`{round((client.latency)*1000, 2)}ms`")
         )
     
+    # channel_id から Channel オブジェクトを取得
+    channel = client.get_channel(payload.channel_id)
+
+    # 該当のチャンネル以外はスルー
+    if channel.id != ID_CHANNEL_README:
+        return
+
+    # guild_id から Guild オブジェクトを取得
+    guild = client.get_guild(payload.guild_id)
+
+    # user_id から Member オブジェクトを取得
+    member = guild.get_member(payload.user_id)
+
+    # 用意した役職IDから Role オブジェクトを取得
+    role = guild.get_role(ID_ROLE_WELCOME)
+
+    # リアクションを付けたメンバーに役職を付与
+    await member.add_roles(role)
+
+    # 分かりやすいように歓迎のメッセージを送る
+    m = await channel.send(f'{role.mention}を{member.mention}に付与！')
+    await asyncio.sleep(5)
+    await m.delete()
 
 
 
