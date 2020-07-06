3c3ca2313dd028283356d7767f70b62a0902609c
amano2 <66359410+amano2@users.noreply.github.com>
2020-07-06 15:06:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,15 +4,7 @@ import random
 import os
 import re
 import ast
-import gspread
-import json
-import redis
-from oauth2client.service_account import ServiceAccountCredentials 
-from flask import Flask, render_template, g
-from hamlish_jinja import HamlishExtension
-from werkzeug import ImmutableDict
-from flask_sqlalchemy import SQLAlchemy
-import traceback
+
 from datetime import datetime, timedelta, timezone
 JST = timezone(timedelta(hours=+9), 'JST')
 TOKEN = os.environ['TOKEN']
@@ -22,26 +14,7 @@ client = discord.Client()
 ID_CHANNEL_README = 725486353151819899 # 該当のチャンネルのID
 ID_ROLE_WELCOME = 719176372773453835 # 付けたい役職のID
 
-class FlaskWithHamlish(Flask):
-    jinja_options = ImmutableDict(
-        extensions=[HamlishExtension]
-    )
-app = FlaskWithHamlish(__name__)
 
-db_uri = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-db = SQLAlchemy(app)
-
-class Entry(db.Model):
-    # テーブル名を定義
-    __tablename__ = "entries"
-
-    # カラムを定義
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(), nullable=False)
-    body = db.Column(db.String(), nullable=False)
-
-    
     
 @client.event
 async def on_message(message):
@@ -62,20 +35,7 @@ async def on_message(message):
     if message.content.startswith("^^adddb "):
         key = message.content.split(" ")[1]
         value = message.content.split(" ")[2]
-        # DBヘ接続
-        dsn = os.environ.get('DATABASE_URL')
-        conn = psycopg2.connect(dsn)
-        cur = conn.cursor()
-        #insert文実行(適宜読み替えてください)
-        cur.execute('BEGIN')
-        cur.execute("insert into entries values(1,'title', 'body', 'date:XX:YY:ZZ')")
-        cur.execute("insert into entries values(2,'title', 'body', 'date:XX:YY:ZZ')")
-        # データを取得する
-        cur.execute('SELECT * FROM entries')
-        for row in cur:
-            print(row[0])
-        cur.execute('COMMIT')
-        exit()        
+
         
         
     if message.content.startswith("^^getdb "):
